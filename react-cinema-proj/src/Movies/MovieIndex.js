9369f7c939f1a91ad1a17f2b891364c8a2e445eb
Filip2137 <filip.andrejuk@gmail.com>
2021-12-06 22:23:48 +0100

    procenty jak pan jezus

--- react-cinema-proj/src/Movies/MovieIndex.js
+++ react-cinema-proj/src/Movies/MovieIndex.js
@@ -18,7 +18,7 @@ export function MovieIndex(params) {
     return (
         <div class="all">
             <Movie movie={movie} index={true}></Movie>
-            <div> 
+            <div class="editable"> 
                 <div><Link to={`edit`}><button class="button1">Edit</button></Link></div>
                 <div>{params.deleteMovie !== undefined ? <button class="button2" onClick={deleteMovie}>Usuń</button> : ""}</div>
                 {deleted === true ? <Navigate replace to="/" /> : ""}
